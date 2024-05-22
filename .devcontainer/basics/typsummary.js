8784d635d8878c1260f632e510f59cd377625396
akhil-0gif <82773654+akhil-0gif@users.noreply.github.com>
2024-05-22 11:28:16 +0000

    cofig

--- .devcontainer/basics/typsummary.js
+++ .devcontainer/basics/typsummary.js
@@ -2,7 +2,7 @@
 
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
 
-const score = 100
+/*const score = 100
 const scoreValue = 100.3
 
 const isLoggedIn = false
@@ -36,4 +36,27 @@ console.log(typeof heros);
 console.log(typeof myFunction);
 console.log(typeof myObj);
 
-// https://262.ecma-international.org/5.1/#sec-11.4.3
+ https://262.ecma-international.org/5.1/#sec-11.4.3*/
+
+
+//************************************************************** 
+
+// let myName= "Akhil"
+//  let anotherName=(myName)
+//  anotherName="Shivi"
+//  console.log(myName);
+//  console.log(anotherName);
+
+
+ let user1 = {
+    email:"akhil.com",
+    password:3421313
+
+}
+ 
+let user2 =user1;
+
+user1.email="aksd.com"
+
+console.log(user1);
+console.log(user2);
