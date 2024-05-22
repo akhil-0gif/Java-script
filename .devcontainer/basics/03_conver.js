5febc303bad793079b76d105d53c3b3f65d5e489
akhil-0gif <82773654+akhil-0gif@users.noreply.github.com>
2024-05-22 07:42:19 +0000

    conversion of datatypes

---
+++ .devcontainer/basics/03_conver.js
@@ -0,0 +1,52 @@
+/*let score="133"// It was in String because of commas  then we convert it into the  number using " valueInNumber"
+console.log(typeof score)
+ let valueInNumber=Number(score)
+ console.log(typeof valueInNumber)
+ console.log(valueInNumber);
+
+
+ let Shi="Ak123"  // mix of string and num  we convert to  num using ValueInNum1 and the output is NAN(Not a Num)
+ console.log(typeof Shi);
+ let valueInNumber1=Number(Shi)
+ console.log(typeof valueInNumber1)
+console.log(valueInNumber1);
+
+let Sh=null // 
+ console.log(typeof Sh);
+ let valueInNumber2=Number(Sh)
+ console.log(typeof valueInNumber2)
+console.log(valueInNumber2);
+
+let ah=undefined  // 
+ console.log(typeof ah);
+ let valueInNumber3=Number(ah)
+ console.log(typeof valueInNumber3)
+console.log(valueInNumber3);
+
+console.table([valueInNumber,valueInNumber1,valueInNumber2,valueInNumber3]);
+
+let isLoggedIn=1
+let BolleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(typeof BolleanIsLoggedIn);
+console.log(BolleanIsLoggedIn);*/
+/*let isLoggedOut=false//(conversion of boolean to number, type)
+let BolleanIsLoggedOut=Boolean(isLoggedOut)
+let valueInNumber4=Number(isLoggedOut)
+console.log(valueInNumber4);
+console.log(typeof BolleanIsLoggedOut);
+console.log(BolleanIsLoggedOut);*/
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+/*let abc=33
+let valueInString=String(abc)
+console.log(typeof valueInString);
+console.log(valueInString);*/
+
+
+
+
+
+
---
+++ .devcontainer/basics/var.js
@@ -0,0 +1,10 @@
+const accountId=1234
+let accountEmail="Akhil@gmail.com"
+var accountPassword="55005500"
+accountCity="Jaipur"
+
+// accountId="236523"
+accountEmail="ZSZS.com"
+accountPassword="1233"
+accountCity="Ludhiana"
+console.table([accountId,accountEmail,accountPassword,accountCity]);
