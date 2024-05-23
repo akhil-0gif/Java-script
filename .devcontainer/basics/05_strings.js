d456711bd9ea48b925612d66881af0e874f8387b
akhil-0gif <82773654+akhil-0gif@users.noreply.github.com>
2024-05-23 07:14:54 +0000

    config of strings

---
+++ .devcontainer/basics/05_strings.js
@@ -0,0 +1,37 @@
+const user=new String("   Hi honey How_are_you. I hope you-get this_message   ")
+// console.log(user);
+
+const ak="akhi"
+const bj="Love"
+// console.log(`Hi my name is ${ak} and i really ${bj} you !`);
+
+// console.log(user[4]);
+// console.log(user.__proto__);
+
+// console.log(user.indexOf("a"));// simply using index of method in strings 
+
+
+let index=2;// we can also define index first then use the .at() method to get the result
+// console.log(user.at(index));
+
+// console.log(user.charAt(6));
+
+// console.log(`so the ${user.charCodeAt(4)} is equal to ${user.charAt("4")}`)
+
+//  console.log(user.length);
+
+// console.log(user.toUpperCase());
+
+// console.log(user.toLowerCase());
+
+// console.log(user.slice(0,6));
+
+// console.log(user.substr(0,5));
+
+// console.log(user.trim("  "));
+
+// const url="https//.akhil@gmail. com//qeea"
+
+// console.log(url.replace(" "));
+
+// console.log(user.split("_"));
