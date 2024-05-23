fd5bdfa7138da1ee9dca65367bfd4c1df18c035d
akhil-0gif <82773654+akhil-0gif@users.noreply.github.com>
2024-05-23 08:16:20 +0000

    config maths function

--- .devcontainer/basics/06_math_num.js
+++ .devcontainer/basics/06_math_num.js
@@ -1,2 +1,42 @@
-const use=new Number(12345)
-console.log(use)
+const use=new Number(23.613)
+// console.log(use)
+
+// console.log(use.toString().length);
+// console.log(Math.exp(1));
+
+// console.log(use.toFixed(1));
+
+// console.log(use.toLocaleString('en-IN'));
+
+// console.log(use.toPrecision(3));
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                 //Maths Functions
+
+// console.log(Math);
+
+// console.log(Math.exp(1));
+
+// console.log(Math.abs(-3));
+
+// console.log(Math.ceil(1.1));
+
+// console.log(Math.floor(1.99));
+
+// console.log(Math.min(34,46,3,23));
+
+// console.log(Math.max(23,35,6,454,223));
+
+// console.log(Math.round(1.4));
+
+// console.log(Math.random());
+
+// console.log(Math.floor(Math.random() * 10 +1 ));
+
+let min= 50;
+let max=100;
+
+console.log(Math.floor(Math.random() * (min-max+1) * 10 + min));
+
+
+
